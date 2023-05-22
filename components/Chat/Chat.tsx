7861885c5d61256de81723352375c5c81a0b8079
Haonan Zhang <zchiowal@gmail.com>
2023-05-22 12:27:13 +0800

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -163,17 +163,6 @@ export const Chat: FC<Props> = memo(
               </div>
             </div>
             <div className="text-center text-gray-500 dark:text-gray-400">
-              <div className="mb-2">
-
-              </div>
-              <div className="mb-2">
-
-              </div>
-              <div className="mb-2">
-                {t(
-            
-                )}
-              </div>
               <div>
                 {t(
                   "全新升级版本网址：",
